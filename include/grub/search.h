501ab3ff84ab29e49a717abfc97e21e52379635f
Carlo Caione <carlo@endlessm.com>
2016-06-10 19:33:51 +0200

    search: Add search by filesystem type
    Add a new search.fs_type tool to search by Partition Type GUID.
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- include/grub/search.h
+++ include/grub/search.h
@@ -23,6 +23,8 @@ void grub_search_fs_file (const char *key, const char *var, int no_floppy,
 			  char **hints, unsigned nhints);
 void grub_search_fs_uuid (const char *key, const char *var, int no_floppy,
 			  char **hints, unsigned nhints);
+void grub_search_fs_type (const char *key, const char *var, int no_floppy,
+			  char **hints, unsigned nhints);
 void grub_search_label (const char *key, const char *var, int no_floppy,
 			char **hints, unsigned nhints);
 
