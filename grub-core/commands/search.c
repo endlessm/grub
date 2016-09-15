endlessm/grub

commit 314f9af83a34fdb160841b4a8c5217f176818fa5

search: Do not try to autoload fs modules when secure booted
When the file/uuid/... is not found on the disk/partition for which we
already have the fs module loaded, the current behaviour is to try to
autoload all the fs modules to access other disks/partitions.

This is obviously going to fail when we have secure boot enabled since
we cannot actively load any external module. The result is an annoying
error message `error: Secure Boot forbids loading module from
(memdisk)/boot/grub/x86_64-efi/...`

We fix this avoiding to autoload the fs modules when the search fails
and we booted in secure mode.

Please note that probably the most correct way to fix this would be
directly to avoid read_lists() (in grub-core/normal/main.c) when secure
boot is enabled, but for now we limit the fix to the search command.

Signed-off-by: Carlo Caione <carlo@endlessm.com>

grub-core/commands/search.c
@@ -348,6 +348,9 @@ FUNC_NAME (const char *key, const char *var, int no_floppy,
       /* Restore autoload hook.  */
       grub_fs_autoload_hook = saved_autoload;
 
+#ifdef GRUB_MACHINE_EFI
+      if (!grub_efi_secure_boot ())
+#endif
       /* Retry with autoload if nothing found.  */
       if (grub_errno == GRUB_ERR_NONE && ctx.count == 0)
 	try (&ctx);

commit 444457e3a5b3a5b467830e58149674fe6d187ad9

search: Add missing header
Fixes: 314f9af83a34fdb160841b4a8c5217f176818fa5
Signed-off-by: Carlo Caione <carlo@endlessm.com>

grub-core/commands/search.c
@@ -30,6 +30,7 @@
 #include <grub/i18n.h>
 #include <grub/disk.h>
 #include <grub/partition.h>
+#include <grub/efi/efi.h>
 
 GRUB_MOD_LICENSE ("GPLv3+");
 