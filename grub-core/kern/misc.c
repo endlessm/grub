ad0f6e784a781b4527f8425250e664d1f700c8b4
Daniel Axtens <dja@axtens.net>
2021-01-13 22:19:01 +1100

    [DEB] kern/misc: Always set *end in grub_strtoull()
    Currently, if there is an error in grub_strtoull(), *end is not set.
    This differs from the usual behavior of strtoull(), and also means that
    some callers may use an uninitialized value for *end.
    
    Set *end unconditionally.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>
    
    Patch-Name: 2021-02-security/064-kern-misc-Always-set-end-in-grub_strtoull.patch

--- grub-core/kern/misc.c
+++ grub-core/kern/misc.c
@@ -406,6 +406,10 @@ grub_strtoull (const char *str, char **end, int base)
 	{
 	  grub_error (GRUB_ERR_OUT_OF_RANGE,
 		      N_("overflow is detected"));
+
+          if (end)
+            *end = (char *) str;
+
 	  return ~0ULL;
 	}
 
@@ -417,6 +421,10 @@ grub_strtoull (const char *str, char **end, int base)
     {
       grub_error (GRUB_ERR_BAD_NUMBER,
 		  N_("unrecognized number"));
+
+      if (end)
+        *end = (char *) str;
+
       return 0;
     }
 
