8f4016a69c02d16c7297af59a9086cd27bea0356
Daniel Axtens <dja@axtens.net>
2021-01-21 18:35:22 +1100

    [DEB] disk/lvm: Do not crash if an expected string is not found
    Clean up a bunch of cases where we could have strstr() fail and lead to
    us dereferencing NULL.
    
    We'll still leak memory in some cases (loops don't clean up allocations
    from earlier iterations if a later iteration fails) but at least we're
    not crashing.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>
    
    Patch-Name: 2021-02-security/087-disk-lvm-Do-not-crash-if-an-expected-string-is-not-found.patch

--- grub-core/disk/lvm.c
+++ grub-core/disk/lvm.c
@@ -538,7 +538,16 @@ error_parsing_metadata:
 			}
 
 		      if (seg->node_count != 1)
-			seg->stripe_size = grub_lvm_getvalue (&p, "stripe_size = ");
+			{
+			  seg->stripe_size = grub_lvm_getvalue (&p, "stripe_size = ");
+			  if (p == NULL)
+			    {
+#ifdef GRUB_UTIL
+			      grub_util_info ("unknown stripe_size");
+#endif
+			      goto lvs_segment_fail;
+			    }
+			}
 
 		      seg->nodes = grub_calloc (seg->node_count,
 						sizeof (*stripe));
@@ -558,7 +567,7 @@ error_parsing_metadata:
 			{
 			  p = grub_strchr (p, '"');
 			  if (p == NULL)
-			    continue;
+			    goto lvs_segment_fail2;
 			  q = ++p;
 			  while (*q != '"')
 			    q++;
@@ -577,7 +586,10 @@ error_parsing_metadata:
 			  stripe->start = grub_lvm_getvalue (&p, ",")
 			    * vg->extent_size;
 			  if (p == NULL)
-			    continue;
+			    {
+			      grub_free (stripe->name);
+			      goto lvs_segment_fail2;
+			    }
 
 			  stripe++;
 			}
@@ -614,7 +626,7 @@ error_parsing_metadata:
 
 			  p = grub_strchr (p, '"');
 			  if (p == NULL)
-			    continue;
+			    goto lvs_segment_fail2;
 			  q = ++p;
 			  while (*q != '"')
 			    q++;
@@ -702,7 +714,7 @@ error_parsing_metadata:
 			  p = p ? grub_strchr (p + 1, '"') : 0;
 			  p = p ? grub_strchr (p + 1, '"') : 0;
 			  if (p == NULL)
-			    continue;
+			    goto lvs_segment_fail2;
 			  q = ++p;
 			  while (*q != '"')
 			    q++;
