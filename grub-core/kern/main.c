70c8a9612fded8988ae0503e4cde3aedf1f42b8b
Michael Chang <mchang@suse.com>
2021-03-18 19:30:26 +0800

    [DEB] i386-pc: build verifiers API as module
    Given no core functions on i386-pc would require verifiers to work and
    the only consumer of the verifier API is the pgp module, it looks good
    to me that we can move the verifiers out of the kernel image and let
    moddep.lst to auto-load it when pgp is loaded on i386-pc platform.
    
    This helps to reduce the size of core image and thus can relax the
    tension of exploding on some i386-pc system with very short MBR gap
    size. See also a very comprehensive summary from Colin [1] about the
    details.
    
    [1] https://lists.gnu.org/archive/html/grub-devel/2021-03/msg00240.html
    
    V2:
    Drop COND_NOT_i386_pc and use !COND_i386_pc.
    Add comment in kern/verifiers.c to help understanding what's going on
    without digging into the commit history.
    
    Reported-by: Colin Watson <cjwatson@debian.org>
    Reviewed-by: Colin Watson <cjwatson@debian.org>
    Signed-off-by: Michael Chang <mchang@suse.com>
    
    Origin: other, https://lists.gnu.org/archive/html/grub-devel/2021-03/msg00251.html
    Bug-Debian: https://bugs.debian.org/984488
    Bug-Debian: https://bugs.debian.org/985374
    Last-Update: 2021-09-24
    
    Patch-Name: pc-verifiers-module.patch

--- grub-core/kern/main.c
+++ grub-core/kern/main.c
@@ -29,7 +29,9 @@
 #include <grub/command.h>
 #include <grub/reader.h>
 #include <grub/parser.h>
+#ifndef GRUB_MACHINE_PCBIOS
 #include <grub/verify.h>
+#endif
 
 #ifdef GRUB_MACHINE_PCBIOS
 #include <grub/machine/memory.h>
@@ -296,8 +298,10 @@ grub_main (void)
 #endif
 #endif
 
+#ifndef GRUB_MACHINE_PCBIOS
   /* Init verifiers API. */
   grub_verifiers_init ();
+#endif
 
   grub_load_config ();
 
