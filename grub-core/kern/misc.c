a8bff308ceda644f77cb72d9145694db04454f8d
Mate Kukri <mate.kukri@canonical.com>
2026-02-10 10:42:37 +0000

    [DEB] Revert "kern: Include function name on debug and error print functions"
    This reverts commit 894241c8543eb0dd27b16962a640f38e9b0c6e19.

--- grub-core/kern/misc.c
+++ grub-core/kern/misc.c
@@ -263,14 +263,14 @@ grub_debug_enabled (const char * condition)
 }
 
 void
-grub_real_dprintf (const char *file, const char *function, const int line, const char *condition,
+grub_real_dprintf (const char *file, const int line, const char *condition,
 		   const char *fmt, ...)
 {
   va_list args;
 
   if (grub_debug_enabled (condition))
     {
-      grub_printf ("%s:%s:%d:%s: ", file, function, line, condition);
+      grub_printf ("%s:%d:%s: ", file, line, condition);
       va_start (args, fmt);
       grub_vprintf (fmt, args);
       va_end (args);
